a27c8a70a118430188e6e6b03f9f5e8a589624f6
ViníciusRibeiroLopes <vi179nicius@gmail.com>
2024-11-08 23:49:57 -0300

    add add

---
+++ usuarios/add.php
@@ -0,0 +1,76 @@
+<?php
+require_once('functions.php');
+add();
+?>
+
+<?php include(HEADER_TEMPLATE); ?>
+
+<br>
+<h2>Novo Funcionário</h2>
+
+<form action="add.php" method="post" enctype="multipart/form-data">
+    <!-- área de campos do formulário -->
+    <hr />
+    <div class="row">
+        <div class="form-group col-md-7">
+            <label for="name">Nome / Razão Social</label>
+            <input type="text" class="form-control" name="usuario['nome']" maxlength="50" required>
+        </div>
+
+        <div class="form-group col-md-2">
+            <label for="campo3">Usuário (Login)</label>
+            <input type="text" class="form-control" name="usuario['user']" disabled>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="form-group col-md-5">
+            <label for="campo1">Senha</label>
+            <input type="text" class="form-control" name="usuario['password']"  required>
+        </div>
+
+        <div class="form-group col-md-2">
+            <label for="campo2">Foto</label>
+            <input type="file" class="form-control" name="foto" id="foto" onchange="previewImage(event)">
+        </div>
+        <div class="form-group col-md-2">
+            <?php
+            if (empty($usuario['foto'])) {
+                $imagem = 'SemImagem.png';
+            } else {
+                $imagem = $usuario['foto'];
+            }
+            ?>
+            <br>
+            <img id="imagePreview" src="../fotos/<?php echo $imagem; ?>" alt="Foto do funcionário" class="img-fluid img-thumbnail" style="width: 180px; height: auto;">
+        </div>
+    </div>
+
+    <br>
+    <div id="actions" class="row">
+        <div class="col-md-12">
+            <button type="submit" class="btn btn-primary"><i class="fa-solid fa-floppy-disk"></i> Salvar</button>
+            <a href="index.php" class="btn btn-default"><i class="fa-solid fa-xmark"></i> Cancelar</a>
+        </div>
+    </div>
+</form>
+
+
+<script>
+    function previewImage(event) {
+        const input = event.target;
+        const preview = document.getElementById('imagePreview');
+
+        if (input.files && input.files[0]) {
+            const reader = new FileReader();
+
+            reader.onload = function(e) {
+                preview.src = e.target.result; // Atualiza a imagem para o preview
+            }
+
+            reader.readAsDataURL(input.files[0]); // Lê o arquivo como uma URL de dados
+        }
+    }
+</script>
+
+<?php include(FOOTER_TEMPLATE); ?>
