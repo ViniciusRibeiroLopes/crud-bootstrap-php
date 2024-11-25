e241e47a42053a39d911b8ca15ebadbf70fbc393
ViníciusRibeiroLopes <vi179nicius@gmail.com>
2024-11-25 19:37:57 -0300

    change login

--- inc/login.php
+++ inc/login.php
@@ -3,30 +3,37 @@
 include('../config.php');
 include(HEADER_TEMPLATE);
 ?>
-<div id="actions" class="mt-5 mb-5">
-    <form action="valida.php" method="post">
-        <div class="row">
-            <!-- User input -->
-            <div class="form-floating col-12 mb-2">
-                <input type="text" class="form-control" id="log" placeholder="Usuário" name="login">
-                <label for="log">Usuário</label>
-            </div>
-            <!-- Password input -->
-            <div class="form-floating col-12 mb-2">
-                <input type="password" class="form-control" id="pass" placeholder="Senha" name="senha">
-                <label for="pass">Senha</label>
-            </div>
-            <!-- Submit button -->
-            <div class="col-12 mb-2">
-                <button type="submit" class="btn btn-secondary btn-block mb-4">
-                    <i class="fa-solid fa-user-check"></i> Conectar
-                </button>
-                <a href="<?php echo BASEURL; ?>" class="btn btn-light btn-block mb-4">
-                    <i class="fa-solid fa-rotate-left"></i> Cancelar
-                </a>
+<div id="login-form" class="container mt-5 mb-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6 col-lg-4">
+            <div class="card shadow-sm">
+                <div class="card-body">
+                    <h2 class="text-center mb-4">Login</h2>
+                    <form action="valida.php" method="post">
+                        <!-- User input -->
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="log" placeholder="Usuário" name="login" required>
+                            <label for="log">Usuário</label>
+                        </div>
+                        <!-- Password input -->
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control" id="pass" placeholder="Senha" name="senha" required>
+                            <label for="pass">Senha</label>
+                        </div>
+                        <!-- Submit button -->
+                        <div class="d-grid gap-2 mb-3">
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fa-solid fa-user-check"></i> Conectar
+                            </button>
+                            <a href="<?php echo BASEURL; ?>" class="btn btn-light">
+                                <i class="fa-solid fa-rotate-left"></i> Cancelar
+                            </a>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </form>
+    </div>
 </div>
 
 <?php include(FOOTER_TEMPLATE); ?>
